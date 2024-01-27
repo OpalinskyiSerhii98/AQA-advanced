dea37585404b25ddb37c5037dfc952936170a1de
Oleh <oleh.opalinskyi@gmail.com>
2024-01-27 13:36:28 +0200

    corrections for task3

--- JS_Conditions_and_Loops/task3.js
+++ JS_Conditions_and_Loops/task3.js
@@ -1,13 +1,15 @@
 const number = 5
 
 for (let i = 1; i <= 10; i++) {
-    console.log(i * number)
+    const sum = i * number
+    console.log(number + ' * ' + i + ' = ' + sum)
 }
 
 const number2 = 2
 let j = 1
 
 while (j <= 10) {
-    console.log(j * number2)
+    const sum2 = j * number2
+    console.log(number2 + ' * ' + j + ' = ' + sum2)
     j++
 }
