be897d8335a08837ed199607aa928c2e00abdf44
Oleh <oleh.opalinskyi@gmail.com>
2024-02-09 11:23:48 +0200

    Add JS_Objects

---
+++ JS_Objects/task2.js
@@ -0,0 +1,10 @@
+const book = {
+    title: 'Important years',
+    author: 'Meg J',
+    year: 2020
+}
+
+const {title, author} = book
+
+console.log(title)
+console.log(author)
---
+++ JS_Objects/task4.js
@@ -0,0 +1,9 @@
+const person = {
+    firstName: 'Antony',
+    lastName: 'Green',
+    age: 28
+}
+
+person.email = 'antonyG@mail.com'
+delete person.age
+console.log(person)
