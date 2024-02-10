8d185bd719751884cc198d23406c13c35fe28ed0
Serhii <opalinskyiserhii@gmail.com>
2024-02-10 15:35:12 +0200

    correction for task1/Objects

--- JS_Objects/task1.js
+++ JS_Objects/task1.js
@@ -1,8 +1,10 @@
 const student = {
-    firstName: 'Antony',
-    lastName: 'Green',
-    age: 25,
-    courses: ["Math", "History", "Programing"]
+
 }
+
+student.firstName = 'Antony'
+student.lastName = 'Green'
+student.age = 25
+student.courses = ["Math", "History", "Programing"]
 
 console.log(student)
